d0b574434dbe79390089835d8664b4e094d813a3
iolado <oladokui@oregonstate.edu>
2025-12-09 22:06:41 -0800

    secure the vault

---
+++ index.js
@@ -0,0 +1,15 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = (100 - 20) / 2;
+
+let code3 = 40 % 39 + 38;
+
+let message = "The vault is secured. The combination is:";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message, codeA, codeB);
